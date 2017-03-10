5914a456d7901894d0974ce24486d7c4a6a3a5b5
Masashi Tanaka <tanaka@seesaa.co.jp>
2017-03-10 20:01:03 +0900

    Initial Commit

---
+++ iBeaconManager/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  iBeaconManager
+//
+//  Created by Masashi Tanaka on 2017/03/10.
+//  Copyright © 2017年 Masashi Tanaka. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
